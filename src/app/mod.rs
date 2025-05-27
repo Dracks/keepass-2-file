553cd313a61d9e4e31870b76a930c8ee8f3afc3b
der-pixel <der-pixel@outlook.com>
2025-05-27 21:31:17 +0200

    fix: normalize function added + test_join_relative_basic fixed on windows

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -52,6 +52,7 @@ impl ErrorCode {
         }
     }
 }
+
 fn join_relative(current: &Path, file: String) -> PathBuf {
     if let Some(without_rel_path) = file.strip_prefix("./") {
         join_relative(current, String::from(without_rel_path))
@@ -397,10 +398,11 @@ mod tests {
 
     #[test]
     fn test_join_relative_basic() {
-        let test_path = Path::new("/home/users/devel/");
-        let relative_path = String::from("./../.././file");
-        let result = join_relative(&test_path, relative_path);
-        assert_eq!(result.to_str().unwrap(), "/home/file");
+        let test_path = tools::normalize_path("test_resources/test_db.kdbx");
+        let absolute_path: PathBuf = Path::new("src/app").canonicalize().unwrap();
+        let relative_path = String::from("./../../test_resources/test_db.kdbx");
+        let joint_path = tools::normalize_path(join_relative(&absolute_path, relative_path));
+        assert_eq!(joint_path, test_path);
     }
 
     #[test]
@@ -557,8 +559,8 @@ mod tests {
             Cli {
                 command: Commands::Config(ConfigCommands::AddFile {
                     name: Some(String::from("New name")),
-                    template: String::from("./test_resources/.env.example"),
-                    output: String::from("./test_resources/tmp/.env"),
+                    template: tools::normalize_path("./test_resources/.env.example"),
+                    output: tools::normalize_path("./test_resources/tmp/.env"),
                     relative_to_input: false,
                 }),
                 config: Some(String::from(test.get_file_path())),
--- src/app/test_helpers.rs
+++ src/app/test_helpers.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 pub mod tests {
     use super::super::config::GlobalConfig;
+    use super::super::tools;
     use super::super::IOLogs;
     use std::cell::RefCell;
 
